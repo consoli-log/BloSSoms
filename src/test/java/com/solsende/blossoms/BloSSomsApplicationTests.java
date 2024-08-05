2a6839c4a21e73b2b05000b37eaad39f05cc9c4b
consoli.log <eumsoli@gmail.com>
2024-08-05 15:51:21 +0900

    :tada: Begin Project

---
+++ src/test/java/com/solsende/blossoms/BloSSomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solsende.blossoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloSSomsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
